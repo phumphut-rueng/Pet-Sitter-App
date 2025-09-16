8133fcdd961a209a4c14918b6aec10dc40113fa6
Phumphut <phumphut.rueng@gmail.com>
2025-09-16 17:24:52 +0700

    feat: setup project

---
+++ src/pages/api/users.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prima";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method === "GET") {
+      const users = await prisma.user.findMany();
+      return res.status(200).json(users);
+    }
+
+    // ถ้า request method อื่น ๆ
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
