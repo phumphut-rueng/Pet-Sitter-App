db05174919897af724288ddada43d6aa043fc180
kanyaweeN <40629441+kanyaweeN@users.noreply.github.com>
2025-09-18 22:06:00 +0700

    feat:delete comment text on reject confirmation

--- src/components/modal/RejectConfirmation.tsx
+++ src/components/modal/RejectConfirmation.tsx
@@ -16,57 +16,6 @@ export default function RejectConfirmation({
     onOpenChange,
     onConfirm,
 }: ConfirmationProps) {
-    // return (
-    //     <AlertDialog open={open} onOpenChange={onOpenChange}>
-    //         <AlertDialogContent className="p-0 rounded-xl w-[400px] font-[700] border-transparent">
-    //             {/* Header */}
-    //             <div className="p-3 pl-5 flex justify-between items-center border-b border-gray-2">
-    //                 <AlertDialogTitle className="h4">
-    //                     Reject Confirmation
-    //                 </AlertDialogTitle>
-    //                 <AlertDialogCancel
-    //                     className="body-sm border-transparent shadow-transparent
-    //                     hover:text-gray-4"
-    //                 >
-    //                     ✕
-    //                 </AlertDialogCancel>
-    //             </div>
-
-    //             <div className="pl-5 pr-4 pb-4">
-    //                 {/* Description */}
-    //                 <AlertDialogDescription className="pb-3 body-sm">
-    //                     Are you sure to reject this pet sitter?
-    //                 </AlertDialogDescription>
-
-    //                 <AlertDialogDescription>
-    //                     <span
-    //                         className="caption text-black">
-    //                         Reason and suggestion
-    //                     </span>
-    //                     <textarea className="w-full border border-gray-4"></textarea>
-    //                 </AlertDialogDescription>
-
-    //                 {/* Actions */}
-    //                 <div className="flex justify-between gap-4 mt-4">
-    //                     <AlertDialogCancel asChild>
-    //                         <Button
-    //                             variant="outline"
-    //                             className="bg-orange-1 text-orange-5 hover:bg-orange-2 border-0"
-    //                         >
-    //                             Cancel
-    //                         </Button>
-    //                     </AlertDialogCancel>
-    //                     <Button
-    //                         className="bg-orange-5 hover:bg-orange-6 text-white"
-    //                         onClick={onConfirm}
-    //                     >
-    //                         Reject
-    //                     </Button>
-    //                 </div>
-    //             </div>
-    //         </AlertDialogContent>
-    //     </AlertDialog>
-    // );
     return (
         <AlertConfirm
             open={open}
