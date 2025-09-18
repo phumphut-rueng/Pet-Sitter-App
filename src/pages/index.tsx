27e32ac4ba5f878f87b4b60e4ec2d7f00a8055bd
Shin <sirapop1998@gmail.com>
2025-09-18 16:10:29 +0700

    refactor: remove unused ChatContainer and clean up Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
 import Navbar from "@/components/Navbar";
 
 
-import Header from '../components/Header';
-import HeroSection from '../components/HeroSection';
-import ChatContainer from '../components/Chat/ChatContainer';
-import Testcss from './testcss';
-
 // Sample chat messages
 
 
