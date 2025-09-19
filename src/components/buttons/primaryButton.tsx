76cf7b4deccdae11568d2a8bddff8c9e83c8f0d0
Phumphut <phumphut.rueng@gmail.com>
2025-09-19 12:15:20 +0700

    feat: add Radix UI components and implement pet type selection in search sitter widget

--- src/components/buttons/primaryButton.tsx
+++ src/components/buttons/primaryButton.tsx
@@ -7,6 +7,8 @@
       textColor = "black"
       className="px-16"   
       //สามารถเพิ่มเขียน override className ได้ เช่น เเก้ความยาวปุ่มด้วย px
+      type?: "button" | "submit" | "reset" เลือก type ของปุ่มะเอาไว้ใช้งานได้
+      onClick={() => ("")}
       /> */}
 
 const bgColorMap = {
@@ -29,6 +31,8 @@ type ButtonProps = {
   bgColor?: keyof typeof bgColorMap;
   textColor?: keyof typeof textColorMap;
   className?: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
 };
 
 export default function PrimaryButton({
@@ -37,17 +41,21 @@ export default function PrimaryButton({
   textColor = "white",
   srcImage,
   className = "",
+  type = "button",
+  onClick,
 
 }: ButtonProps) {
   return (
     <button
       className={`${bgColorMap[bgColor]} ${textColorMap[textColor]} h-12 px-10 rounded-full flex items-center text-base font-bold hover:cursor-pointer active:scale-95 transition ${className}`}
+      onClick={onClick}
+      type={type}
     >
       {srcImage && (
         <img
           src={srcImage}
           alt="icon"
-          className="inline-block mr-2 w-6 h-6"
+          className="inline-block mr-2 w-6 h-6 "
         />
       )}
       {text}
