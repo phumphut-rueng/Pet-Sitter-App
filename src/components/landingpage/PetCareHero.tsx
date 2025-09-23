92a9079cfa8da41d7a78ff27c338c8c48a59eaac
Shin <sirapop1998@gmail.com>
2025-09-24 05:52:23 +0700

    fix: Update import path for ServicesSection and adjust formatting in PetCareHero component for improved readability

--- src/components/landingpage/PetCareHero.tsx
+++ src/components/landingpage/PetCareHero.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Image from "next/image";
-import ServicesSection from "./ServicesSection";
+import ServicesSection from "@/components/landingpage/ServicesSection";
 import { satoshi } from "@/fonts";
 
 const PetCareHero = () => {
   return (
-    <div className={`${satoshi.className} container mx-auto py-16 px-4 sm:px-6 lg:px-8`}>
+    <div
+      className={`${satoshi.className} container mx-auto py-16 px-4 sm:px-6 lg:px-8`}
+    >
       {/* Main Heading */}
       <div className="text-center mb-16">
         <h1 className="text-2xl sm:text-3xl font-bold text-black leading-tight">
-           "Your Pets, Our Priority: Perfect Care, Anytime, Anywhere."
+          &quot;Your Pets, Our Priority: Perfect Care, Anytime, Anywhere.&quot;
         </h1>
       </div>
 
@@ -22,19 +24,18 @@ const PetCareHero = () => {
 
         {/* Right Section - Image */}
         <div className="w-full lg:w-1/2 flex items-center justify-center">
-            <div className="w-80 h-fit lg:w-96 lg:h-fit flex items-center justify-center relative">
-              {/* Cat Image */}
-              <div className="relative z-10">
-                <Image
-                  src="/images/landing_page/lovely-pet-portrait-isolated.svg"
-                  alt="Cat being petted"
-                  width={400}
-                  height={400}
-                  className="object-contain"
-                />
-              </div>
+          <div className="w-80 h-fit lg:w-96 lg:h-fit flex items-center justify-center relative">
+            {/* Cat Image */}
+            <div className="relative z-10">
+              <Image
+                src="/images/landing_page/lovely-pet-portrait-isolated.svg"
+                alt="Cat being petted"
+                width={400}
+                height={400}
+                className="object-contain"
+              />
             </div>
-          
+          </div>
         </div>
       </div>
     </div>
