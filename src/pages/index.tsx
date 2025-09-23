582efcb2057719d0ee15d36562440b684cb69f81
Shin <sirapop1998@gmail.com>
2025-09-23 14:10:54 +0700

    feat: Refactor Home page to use LandingPage component, consolidating layout and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,10 @@
-import Navbar from "@/components/navbar/Navbar";
-import HeroSection from "@/components/HeroSection";
-import SearchSitter from "@/components/widgets/searchSitter";
-import PetCareHero from "@/components/PetCareHero";
-import ThreeSectionLayout from "@/components/ThreeSectionLayout";
-import PetSitterHero from "@/components/PetSitterHero";
-import Footer from "@/components/Footer";
+import LandingPage from "./landingpage";
 
 export default function Home() {
   return (
     <>
       <div className="container-1200">
-        <Navbar />
-        <HeroSection />
-        <SearchSitter />
-        <PetCareHero />
-        <ThreeSectionLayout />
-        <PetSitterHero />
-        <Footer />
+        <LandingPage />
       </div>
     </>
   );
