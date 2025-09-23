4164b8f25adf37215ba7910ee768aa9b3d802f81
Shin <sirapop1998@gmail.com>
2025-09-24 05:31:18 +0700

    feat: Add new landing page components including Footer, HeroSection, PetCareHero, PetSitterHero, ServicesSection, and ThreeSectionLayout for enhanced layout and user experience

--- src/pages/landingpage.tsx
+++ src/pages/landingpage.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@/components/navbar/Navbar";
-import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/components/landingpage/HeroSection";
 import SearchSitter from "@/components/widgets/searchSitter";
-import PetCareHero from "@/components/PetCareHero";
-import ThreeSectionLayout from "@/components/ThreeSectionLayout";
-import PetSitterHero from "@/components/PetSitterHero";
-import Footer from "@/components/Footer";
+import PetCareHero from "@/components/landingpage/PetCareHero";
+import ThreeSectionLayout from "@/components/landingpage/ThreeSectionLayout";
+import PetSitterHero from "@/components/landingpage/PetSitterHero";
+import Footer from "@/components/landingpage/Footer";
 
 export default function LandingPage() {
   return (
