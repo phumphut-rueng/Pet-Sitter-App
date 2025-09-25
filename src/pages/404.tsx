63407c6adaf9f9e94cd962b124f4e7aa48b13a22
mookp12 <kraiviwat.p@gmail.com>
2025-09-25 11:38:52 +0700

    fix: add comment for new push

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,6 +4,7 @@ export default function NotFound() {
   return (
     <>
     <Navbar />
+    {/* comment */}
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)]">
       <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
     </div>
