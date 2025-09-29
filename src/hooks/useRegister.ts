4e44a4c3ae265b58ac49a5fe4573503423bf87fd
kanyaweeN <40629441+kanyaweeN@users.noreply.github.com>
2025-09-29 23:30:09 +0700

    feat:refactor register page extract form, role, and api logic into separate hooks

---
+++ src/hooks/useRegister.ts
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import { RegisterForm } from "@/types/register.type"
+import { useRegisterForm } from "./useRegisterForm"
+import { useRegisterRole } from "./useRegisterRole"
+import { useRegisterApi } from "./useRegisterApi"
+
+// รวม logic เรียกใช้ hook ย่อยทั้งหมด
+export function useRegister() {
+    const [isLoading, setIsLoading] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+
+    const { form, setForm, error, setError, handleChange, handlePhoneChange, validateForm } =
+        useRegisterForm()
+
+    const { role, handleChangeRole } = useRegisterRole(setError)
+    const { saveData, addRole } = useRegisterApi(form, setError)
+
+    const saveOwnerData = (newErrors: RegisterForm, role_ids: number[]) => {
+        if (Object.values(newErrors).every((val) => val === "")) {
+            saveData(role_ids)
+        } else {
+            setError(newErrors)
+        }
+    }
+
+    const handleOnConfirm = async () => {
+        await addRole()
+        setIsOpen(false)
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setIsLoading(true)
+
+        const newErrors = await validateForm(role)
+
+        console.log(newErrors);
+
+
+        if (form.email.trim() && role === 3) {
+            if (!newErrors.email) {
+                saveOwnerData(newErrors, [2, 3])
+            } else if (newErrors.email && newErrors.email !== "Conflict") {
+                setIsOpen(true)
+            } else {
+                setError(newErrors)
+            }
+        } else {
+            saveOwnerData(newErrors, [2])
+        }
+
+        setIsLoading(false)
+    }
+
+    return {
+        form,
+        error,
+        role,
+        isLoading,
+        isOpen,
+        setIsOpen,
+        handleChange,
+        handlePhoneChange,
+        handleSubmit,
+        handleChangeRole,
+        handleOnConfirm,
+    }
+}
