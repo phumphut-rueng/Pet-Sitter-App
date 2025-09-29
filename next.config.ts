c9774972498bc2ae874715b79736d021fdebf0b0
Shin <sirapop1998@gmail.com>
2025-09-29 11:37:04 +0700

    fix: Correct formatting in next.config.ts for image handling configuration

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 
   images: {
-    formats: ['image/avif', 'image/webp' ],
+    formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
